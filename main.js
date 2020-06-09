81a9e80bfd0f2e13dcfafa6e2ac8efe9a8cc2af4
Yumi Obuchi <yumiobuchi97@gmail.com>
2020-06-09 09:23:57 -0400

    animation for homepage

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+  "  Hey! Welcome to my corner of the internet.", "",
+  "I'm a software developer & product strategist from NYC.","",
+  " I currently solve financial & digital problems for F500 clients at PwC, and have worked for Marvel as 1 of the 3 core members of their Business Intelligence team","", "I'm also a writer with an emphasis on literary criticism.");
 
-var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  var iSpeed = 10; // time delay of print out
+  var iIndex = 0; // start printing array at this posision
+  var iArrLength = aText[0].length; // the length of the text array
+  var iScrollAt = 20; // start scrolling up at this many lines
+   
+  var iTextPos = 0; // initialise text position
+  var sContents = ''; // initialise contents variable
+  var iRow; // initialise current row
+   
+  function typewriter()
+  {
+   sContents =  ' ';
+   iRow = Math.max(0, iIndex-iScrollAt);
+   var destination = document.getElementById("typedtext");
+   
+   while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+   }
+   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+   if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+     iArrLength = aText[iIndex].length;
+     setTimeout("typewriter()", 10);
+    }
+   } else {
+    setTimeout("typewriter()", iSpeed);
+   }
   }
-}
+  
+  
 
-
-$(document).ready(function () {
-    //smooth scrolling
-    // $('nav a[href*="#"]').on('click',function(){
-    //     $('html,body').animate(keyframes:{
-    //         scrollTop: $($(this).attr('href')).offset().top - 100
-    //     }, options: 2000);
-    // }
-
-    //when click button -- goes to top
-    // $('#up').on('click', function(){
-    //     $('html,body').animate(keyframes:{
-    //         scrollTop: 0
-    //     },options: 2000);
-    // })
-
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: true  //animatino only happens once
-    });
-
-});
-
+typewriter();
